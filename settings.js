b81ed4369003faa944c84d8fdd7dc5061dda01d4
Mitko Georgiev <omnichronous@gmail.com>
2017-11-09 14:16:28 +0200

    Routing all requests through CORS proxy

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 (function () {
     'use strict';
     module.exports = {
+        corsProxy: "https://cors-anywhere.herokuapp.com/",
         headers: { 'User-Agent': 'metalminer/1.0.0 (https://github.com/sjaak666/metalminer; jon.asplund@gmail.com)' },
-        googleAPIKey: 'AIzaSyCTnTnGZUkS6zT3Fny3D4KM5TXq4xtYUE8',
+        googleAPIKey: 'AIzaSyCJTdg_tYcNOQCW8aALLoIWd8AxxbqHH6Y',
         searchEngines: {
-            youtube: '001592416721279294405:lkkf4bxzsvg'
+            youtube: '002963118278184888316:bycoq3alel4'
         }
     };
 }) ();
